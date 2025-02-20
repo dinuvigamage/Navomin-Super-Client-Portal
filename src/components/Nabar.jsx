a8018d6bbc7b635694465f7569bd88e13abcc97b
dinuvigamage <dinuvigamage@gmail.com>
2025-02-20 10:19:36 +0530

    Create the navigation between home and login

--- src/components/Nabar.jsx
+++ src/components/Nabar.jsx
@@ -1,58 +1,76 @@
 import React from "react";
 import { FiMenu } from "react-icons/fi";
 import { FaShoppingCart } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
-const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm p-3">
-    <div className="container-fluid">
-      
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-      >
-        <FiMenu className="fs-3" />
-      </button>
+const Navbar = () => {
+  const navigate = useNavigate(); 
 
-      
-      <a className="navbar-brand fw-bold ms-3 d-flex align-items-center" href="#">
-        <img
-          src="images/Logo.png"  
-          alt="Logo"
-          className="me-2"
-          style={{ width: "150px", height: "40px" }}
-        />
-      </a>
+  const handleLoginSignup = () => {
+    navigate("/login"); 
+  };
 
-      {/* Navbar Links */}
-      <div
-        className="collapse navbar-collapse justify-content-center"
-        id="navbarNav"
-      >
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link text-primary" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Products</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Pre-Order Bakery</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">My Orders</a>
-          </li>
-        </ul>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm p-3">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <FiMenu className="fs-3" />
+        </button>
+
+        <a
+          className="navbar-brand fw-bold ms-3 d-flex align-items-center"
+          href="#"
+        >
+          <img
+            src="images/Logo.png"
+            alt="Logo"
+            className="me-2"
+            style={{ width: "150px", height: "40px" }}
+          />
+        </a>
+
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link text-primary" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Products
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pre-Order Bakery
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                My Orders
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="d-flex align-items-center">
+          <button className="btn btn-primary me-3" onClick={handleLoginSignup}>
+            Login/Signup
+          </button>
+          <FaShoppingCart className="fs-3 text-dark" />
+        </div>
       </div>
-
-      {/* Login Button & Shopping Cart Icon */}
-      <div className="d-flex align-items-center">
-        <button className="btn btn-primary me-3">Login/Signup</button>
-        <FaShoppingCart className="fs-3 text-dark" />
-      </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Navbar;
